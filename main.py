332e4d87fc56676689a0f5630b149961cb0c80f8
Hussain-moayad <hmwyd600@gmail.com>
2026-02-16 04:37:03 -0500

    Add tuple unpacking example in main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+#tuble = cannot change("hussain" , 16 , "baghdad")
+#list = u can chenge ["hussain" , 16 , "baghdad"] 
+hussain = ("hussain" , 16 , "baghdad")
+(name , age , city) = hussain
+print(name)
+print(age)
+print(city) 
